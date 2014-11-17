1a7db8e98167c2a86f3221c57c8d017cd73a247b
Borna Almasi <borna@borna-almasi.com>
2014-11-17 17:12:27 -0500

    Added Sitemap

--- server/routes.js
+++ server/routes.js
@@ -4,7 +4,8 @@
 
 'use strict';
 
-var errors = require('./components/errors');
+var errors = require('./components/errors'),
+  sm = require('sitemap');
 
 module.exports = function(app) {
 
@@ -14,7 +15,23 @@ module.exports = function(app) {
   app.use('/api/newsletter', require('./api/newsletter'));
 
   app.use('/auth', require('./auth'));
-  
+
+//   Sitemap logic
+  var sitemap = sm.createSitemap ({
+    hostname: 'http://tasky.me',
+    cacheTime: 600000,        // 600 sec - cache purge period
+    urls: [
+      { url: '/',  changefreq: 'monthly', priority: 1.0 }
+    ]
+  });
+
+  app.get('/sitemap.xml', function(req, res) {
+    sitemap.toXML( function (xml) {
+      res.header('Content-Type', 'application/xml');
+      res.send( xml );
+    });
+  });
+
   // All undefined asset or api routes should return a 404
   app.route('/:url(api|auth|components|app|bower_components|assets)/*')
    .get(errors[404]);
